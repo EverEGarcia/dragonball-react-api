e1fc1ca8bc1aa2c95335a9b10b7cec807ef1474e
EverEGarcia <everesli89@gmail.com>
2024-09-30 22:28:46 -0600

    Update build files for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dragonball-react-api/', // Set this to your GitHub repository name
+  base: '/https://everegarcia.github.io/dragonball-react-api/', // Add this line
 });
